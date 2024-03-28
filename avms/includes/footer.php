89e245d874981c4b4e61805a456126efb7008d48
thekirankumarv <thekirankumarv@gmail.com>
2024-03-28 12:59:33 +0530

    initial commit

---
+++ avms/includes/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <p>AccessGuard: Advanced Residential Visitor Oversight Solution.</p>
+                                </div>
+                            </div>
+                        </div>
